3d1521998f395e30f1a7a88e6643de3c8036f1f2
Cornelius Dol <cornelius@dolhub.com>
2026-06-22 18:55:06 -0700

    Enhance : UvDom.js to allow subview patching.

--- src/UvVersion.js
+++ src/UvVersion.js
@@ -1,5 +1,5 @@
-const UvVersion = Object.freeze(() => {
+const UvVersion = Object.freeze((() => {
     let VERSION = "1.00_dev"; /*2026.0622.2226*/                                                                            // version (the commented datestamp is when the version was last updated)
     let BUILD   = "2026.0622.2226";                                                                                     // build datestamp (separate from the version datestamp so they can be updated independently)
     return { VERSION, BUILD };
-    });
+    })());
