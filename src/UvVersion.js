e2d1a5d8c05739fc4faedd05cc107c69f73e968d
Lawrence Dol <lawrence@dolhub.com>
2026-06-24 13:48:59 -0700

    Ultraviolet 1.00.

--- src/UvVersion.js
+++ src/UvVersion.js
@@ -1,5 +1,5 @@
 const UvVersion = Object.freeze((() => {
-    let VERSION = "1.00_dev"; /*2026.0622.2226*/                                                                            // version (the commented datestamp is when the version was last updated)
-    let BUILD   = "2026.0622.2226";                                                                                     // build datestamp (separate from the version datestamp so they can be updated independently)
+    let VERSION = "1.00"; /*2026.0623.2259*/                                                                            // version (the commented datestamp is when the version was last updated)
+    let BUILD   = "2026.0623.2259";                                                                                     // build datestamp (separate from the version datestamp so they can be updated independently)
     return { VERSION, BUILD };
     })());
