05a072e256cd5c7c324c85755e29c8c83a221bc7
L.P. Cornelius Dol <cornelius@dolhub.com>
2026-08-01 14:17:09 -0700

    Next version.

--- src/UvVersion.js
+++ src/UvVersion.js
@@ -1,5 +1,5 @@
 const UvVersion = Object.freeze((() => {
-    let VERSION = "1.01"; /*2026.0630.2202*/                                                                            // version (the commented datestamp is when the version was last updated)
+    let VERSION = "1.02"; /*2026.0630.2202*/                                                                            // version (the commented datestamp is when the version was last updated)
     let BUILD   = "2026.0630.2202";                                                                                     // build datestamp (separate from the version datestamp so they can be updated independently)
     return { VERSION, BUILD };
     })());
